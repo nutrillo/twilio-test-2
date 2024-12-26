ab8224e50979e365eee1a073c7fa46977ae9a21a
nutrillo <jorditrillotoro@gmail.com>
2024-12-26 18:29:36 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Configurar OpenAI
 const configuration = new Configuration({
